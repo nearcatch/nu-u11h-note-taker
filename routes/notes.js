fe9f814a9ab93049349a9d58f8f2fdd3cdff097a
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-28 18:02:15 -0500

    DELETE in notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -3,14 +3,17 @@ const { v4: uuidv4 } = require("uuid");
 const path = require("path");
 const notes = require("express").Router();
 
+// GET at /api/notes to get all current notes
 notes.get("/", (req, res) =>
   fs.readFile("./db/db.json", (err, data) => {
     res.json(JSON.parse(data));
-    console.log(JSON.parse(data));
-    err ? console.error(err) : console.log("\nsent current notes to /notes GET");
+    err
+      ? console.error(err)
+      : console.log("\nsent current notes to /notes GET");
   })
 );
 
+// POST at /api/notes to save a new note
 notes.post("/", (req, res) => {
   const { title, text } = req.body;
 
@@ -28,7 +31,9 @@ notes.post("/", (req, res) => {
         const db = JSON.parse(data);
         db.push(newNote);
         fs.writeFile("./db/db.json", JSON.stringify(db, null, 4), (err) =>
-          err ? console.error(err) : console.info(`\nnew note written to db.json`)
+          err
+            ? console.error(err)
+            : console.info(`\nnew note written to db.json`)
         );
         res.json(`new note saved to database`);
       }
@@ -36,6 +41,25 @@ notes.post("/", (req, res) => {
   } else {
     res.error("error in saving note");
   }
+});
+
+// DELETE Route for a specific tip
+notes.delete("/:id", (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.error("error in deleting note");
+    } else {
+      const db = JSON.parse(data);
+      const newDb = db.filter((note) => note.id !== noteId);
+      fs.writeFile("./db/db.json", JSON.stringify(newDb, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nnote deleted from db.json`)
+      );
+      res.json(`note deleted from database`);
+    }
+  });
 });
 
 module.exports = notes;
