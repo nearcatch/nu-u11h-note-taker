88dd089d8dca82fcd4b2af617fc1d4e8b39b7ac3
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-28 14:46:16 -0500

    basic routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// express basic middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// use public
+app.use(express.static("public"));
+
+// send notes.html for /notes
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// listen on port indefinitely
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
