673b3f55229c66bfea4035fb996ea0c23089e186
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-28 17:47:11 -0500

    POST in notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,41 @@
-const notes = require('express').Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const notes = require("express").Router();
 
+notes.get("/", (req, res) =>
+  fs.readFile("./db/db.json", (err, data) => {
+    res.json(JSON.parse(data));
+    console.log(JSON.parse(data));
+    err ? console.error(err) : console.log("\nsent current notes to /notes GET");
+  })
+);
 
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.error("error in saving note");
+      } else {
+        const db = JSON.parse(data);
+        db.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(db, null, 4), (err) =>
+          err ? console.error(err) : console.info(`\nnew note written to db.json`)
+        );
+        res.json(`new note saved to database`);
+      }
+    });
+  } else {
+    res.error("error in saving note");
+  }
+});
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -9,13 +9,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// use public
+// serve public folder tree by default
 app.use(express.static("public"));
+
+// use apiRouter for /api
+app.use('/api', apiRouter);
 
 // send notes.html for /notes
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+// send index.html for misc requests - comment out during testing
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public/index.html"))
+// );
 
 // listen on port indefinitely
 app.listen(PORT, () =>
